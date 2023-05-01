1db4a1819f763e4f9d85f86f90f27bcc5b7a1aad
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-01 16:23:35 -0300

    algo: sqrt search

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount;
+    //if its larger than array length it will break and i will be reduced to i - jumpAmount
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpAmount;
+    //form here it goes 1 by 1
+    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
