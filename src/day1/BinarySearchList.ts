f8154eea01c733790f04870c6c9444155783f5bf
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-01 13:44:30 -0300

    algo: binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,43 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let highestValue: number;
+    let lowestValue: number;
+    let half: number;
+    let order: order;
+    if (haystack.length === 0) return false;
+    if (haystack[haystack.length - 1] > haystack[0]) {
+        highestValue = haystack.length;
+        lowestValue = 0;
+        order = "ASC";
+    } else {
+        highestValue = -1;
+        lowestValue = haystack.length - 1;
+        order = "DESC";
+    }
 
+    do {
+        half = calcHalf(lowestValue, highestValue, order);
+        if (haystack[half] === needle) {
+            return true;
+        } else if (needle > haystack[half]) {
+            if (order === "ASC") {
+                lowestValue = half + 1;
+            } else {
+                lowestValue = half - 1;
+            }
+        } else if (needle < haystack[half]) {
+            highestValue = half;
+        }
+    } while (
+        order === "ASC"
+            ? lowestValue < highestValue
+            : highestValue < lowestValue
+    );
+    return false;
+    function calcHalf(low: number, high: number, order: order) {
+        if (order === "ASC") {
+            return Math.floor(low + (high - low) / 2);
+        }
+        return Math.ceil(high + (low - high) / 2);
+    }
 }
+type order = "ASC" | "DESC";
