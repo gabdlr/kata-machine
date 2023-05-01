9de21ffa28354eccd4ae3429ce089c1188f00ba9
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-01 19:21:50 -0300

    algo: bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
+//(N+1)*(N/2) tells the sum of numbers between a range
 export default function bubble_sort(arr: number[]): void {
-
+    let unsortedElements = arr.length;
+    while (unsortedElements > 0) {
+        for (let i = 0; i < unsortedElements - 1; i++) {
+            let buffer: number;
+            if (arr[i] > arr[i + 1]) {
+                buffer = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = buffer;
+            }
+        }
+        unsortedElements--;
+    }
 }
+
