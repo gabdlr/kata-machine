42d488b06cca2ac52ae5f670dc81a2ea5640f0c1
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-01 22:45:09 -0300

    ds: queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,49 @@
 export default class Queue<T> {
-    public length: number;
-
-    
+    private _length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this._length = 0;
+    }
+
+    get length() {
+        return this._length;
     }
 
     enqueue(item: T): void {
+        this._length++;
+        const node: Node<T> = { value: item };
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        //we make current tail point to the new node
+        this.tail.next = node;
+        //we set the new node as tail
+        this.tail = node;
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) return undefined;
+        this._length--;
+        const head = this.head;
+        //switch head to next node
+        this.head = this.head.next;
+        //free operation, just for demonstration purposes as javascript is gc
+        head.next = undefined;
+        if (this._length === 0) this.tail = undefined;
+        return head.value;
+    }
 
-}
     peek(): T | undefined {
+        return this.head?.value;
+    }
+}
 
-}
-}
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
