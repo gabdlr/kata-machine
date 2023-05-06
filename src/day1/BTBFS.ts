b7f69e3e8b676f9b0b1889cbf841d1ddcceaf404
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-05 22:20:12 -0300

    algo: binary tree comparison

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
+import Queue from "./Queue";
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-
+    const q = new Queue<BinaryNode<number>>();
+    q.enqueue(head);
+    while (q.length) {
+        //remove curr item of the queue
+        const curr: BinaryNode<number> | undefined = q.deque();
+        if (curr?.value === needle) {
+            return true;
+        }
+        //add node children
+        if (curr?.left) {
+            q.enqueue(curr.left);
+        }
+        if (curr?.right) {
+            q.enqueue(curr.right);
+        }
+    }
+    return false;
 }
+
