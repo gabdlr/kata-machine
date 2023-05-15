41bd535389baac6fba19c407cc546fb806b793ae
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-05-14 23:02:32 -0300

    algo: BFS on a graph

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,42 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+
+    const q: Queue<number> = new Queue();
+    q.enqueue(source);
+
+    do {
+        const curr = q.deque() as number;
+        if (curr === needle) {
+            break;
+        }
+        const adjs = graph[curr];
+        for (let i = 0; i < adjs.length; ++i) {
+            seen[curr] = true;
+            if (adjs[i] === 0) continue;
+            if (seen[i]) continue;
+            seen[i] = true;
+            prev[i] = curr;
+            q.enqueue(i);
+        }
+    } while (q.length);
+
+    if (prev[needle] === -1) return null;
+    //build it backwards
+    let curr = needle;
+    const out: number[] = [];
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    return [source].concat([...out].reverse());
 }
+
